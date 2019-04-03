f4627c3f4ac0db9678f6fad9a9e3c2c53b08ac57
Martin Senne <martin.senne@pinuts.de>
2019-04-03 10:22:01 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  
+  async fetchFromAPI() {
+    const body = `<?xml version="1.0" encoding="utf-8"?>
+    <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+      <soap12:Header>
+        <tl_Authentifizierung2 xmlns="wsCalendar3">
+          <TLNamespace>pinuts</TLNamespace>
+          <Login>tlsoap</Login>
+          <Password>ql6i2fhf</Password>
+        </tl_Authentifizierung2>
+      </soap12:Header>
+      <soap12:Body>
+        <FreieTermine2 xmlns="wsCalendar3">
+          <Terminplan>1</Terminplan>
+          <DatumVon_UTC>20190401T000000Z</DatumVon_UTC>
+          <DatumBis_UTC>20190430T000000Z</DatumBis_UTC>
+        </FreieTermine2>
+      </soap12:Body>
+    </soap12:Envelope>`;
+    const rsp = axios
+      .post('https://www.terminland.de/pinuts/tlsoap/default.asmx', body, {
+        headers: { 'Content-Type': 'application/soap+xml' },
+      })
+      .then(rsp => console.log(rsp));
+
+    const response = await rsp;
+
+    return response;
+  }
   render() {
     return (
       <div className="App">
