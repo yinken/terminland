bd07f1da82ae794008ab7ad521c691b677a2ac84
Martin Senne <martin.senne@pinuts.de>
2019-04-03 10:31:47 +0200

    add api request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
 class App extends Component {
-
   
   async fetchFromAPI() {
     const body = `<?xml version="1.0" encoding="utf-8"?>
@@ -34,25 +32,13 @@ class App extends Component {
 
     return response;
   }
+
+  componentDidMount() {
+    this.fetchFromAPI();
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return null;
   }
 }
 
