7ed5620acf7f1d5aba656fff27ae000cd6167d4e
Martin Senne <martin.senne@pinuts.de>
2019-04-03 15:43:12 +0200

    test

--- src/App.js
+++ src/App.js
@@ -4,67 +4,65 @@ import axios from 'axios';
 
 class App extends Component {
   
-  fetchFromAPI() {
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.open(
-      'POST',
-      'https://interface.synxis.com/ChannelConnect.asmx',
-      true
-    );
-
-    // build SOAP request
-    const sr = `<?xml version="1.0" encoding="utf-8"?>
-		<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-			<soap:Header><HTNGHeader xmlns="http://htng.org/1.1/Header/"><From><Credential><userName>deshot</userName><password>trui32hn</password></Credential></From></HTNGHeader></soap:Header>
-			<soap:Body>
-			<OTA_HotelAvailRQ SummaryOnly="true" PrimaryLangID="en" xmlns="http://www.opentravel.org/OTA/2003/05">
-				<AvailRequestSegments>
-					<AvailRequestSegment>
-						<StayDateRange Start="2019-05-04" End="2019-05-05" />
-						<RoomStayCandidates>
-							<RoomStayCandidate Quantity="1">
-								<GuestCounts>
-									<GuestCount AgeQualifyingCode="10" Count="1" />';
-									</GuestCounts>
-							</RoomStayCandidate>
-						</RoomStayCandidates>
-						<HotelSearchCriteria>
-						<Criterion><HotelRef HotelCode="12435" /></Criterion>
-						</HotelSearchCriteria>
-					</AvailRequestSegment>
-				</AvailRequestSegments>
-				<HotelReservationIDs>
-					<HotelReservationID ResID_Type="14" ResID_Value="" />
-				</HotelReservationIDs>
-				<POS>
-				<Source>
-					<BookingChannel Primary="10" />
-					<RequestorId ID="10" />
-				</Source>
-				</POS>
-			</OTA_HotelAvailRQ>
-			</soap:Body>
-		</soap:Envelope>
-`;
-
-    xmlhttp.onreadystatechange = function() {
-      if (xmlhttp.readyState === 4) {
-        if (xmlhttp.status === 200) {
-          console.log(xmlhttp.responseText);
-          // alert('done. use firebug/console to see network response');
-        }
-      }
-    };
-    // Send the POST request
-
-    xmlhttp.setRequestHeader('Content-Type', 'text/xml');
-    xmlhttp.send(sr);
-    // send request
-    // ...
+  soapRequest(url, headers, xml, timeout = 10000) {
+    return new Promise((resolve, reject) => {
+      axios({
+        method: 'post',
+        url,
+        headers,
+        data: xml,
+        timeout,
+      })
+        .then(response => {
+          resolve({
+            response: {
+              body: response.data,
+              statusCode: response.status,
+            },
+          });
+        })
+        .catch(error => {
+          if (error.response) {
+            console.log(`SOAP FAIL: ${error}`);
+            reject(error.response.data);
+          } else {
+            console.log(`SOAP FAIL: ${error}`);
+            reject(error);
+          }
+        });
+    });
   }
 
-  componentDidMount() {
-    this.fetchFromAPI();
+  async componentDidMount() {    
+    const body = `<?xml version="1.0" encoding="utf-8"?>
+    <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+        <soap12:Header>
+            <tl_Authentifizierung2 xmlns="wsCalendar3">
+                <TLNamespace>pinuts</TLNamespace>
+                <Login>tlsoap</Login>
+                <Password>ql6i2fhf</Password>
+            </tl_Authentifizierung2>
+        </soap12:Header>
+        <soap12:Body>
+            <FreieTermine2 xmlns="wsCalendar3">
+                <Terminplan>1</Terminplan>
+                <DatumVon_UTC>20190401T000000Z</DatumVon_UTC>
+                <DatumBis_UTC>20190430T000000Z</DatumBis_UTC>
+            </FreieTermine2>
+        </soap12:Body>
+    </soap12:Envelope>`;
+
+    const headers = {
+      'Content-Type': 'text/xml;charset=UTF-8',
+      soapAction: 'wsCalendar3/FreieTermine2',
+    };
+
+    const { response } = await this.soapRequest(
+      'https://www.terminland.de/pinuts/tlsoap/default.asmx',
+      headers,
+      body
+    );
+    console.log(response);
   }
 
   render() {
